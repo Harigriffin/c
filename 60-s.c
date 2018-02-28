0c995cf7313a15dca0d9e8d7a8458a37a1e71052
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-02-28 14:44:04 +0530

    Create 60-s.c

---
+++ 60-s.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int c=0,n,i,x=1,y=1;
+scanf("%d",&n);
+if(n>=2)
+{
+printf("1\t1\t");
+for(i=1;i<(n-1);i++)
+{
+c=x+y;
+x=y;
+y=c;
+printf("%d\t",c);
+}
+}
+else
+printf("%d",x);
+return 0;
+}

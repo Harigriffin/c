838ec10597e596763a722a4f6c95b4152a3ef648
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-02-28 14:08:07 +0530

    Create 56-s.c

---
+++ 56-s.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100],x=0,y=0,i;
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]>='0'&&a[i]<='9')
+{
+x++;
+}
+if(a[i]>='a'&&a[i]<='z')
+{
+y++;
+}
+}
+if(x!=0&&y!=0)
+printf("YES");
+else
+printf("NO");
+}

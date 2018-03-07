7a2cab524d4668858cf4af31a774c3f12c11675e
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-03-07 14:01:40 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,i,x=0;
+scanf("%d",&a);
+scanf("%d%d",&b,&c);
+for(i=b;i<=c;i++)
+{
+if(a==i)
+x++;
+}
+if(x!=0)
+printf("YES");
+else
+printf("NO");
+}

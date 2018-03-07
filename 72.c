136cde270ca988b7d89500bdd83187ee1a0b6d5a
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-03-07 13:56:28 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int i,c=0;
+scanf("%s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+{
+c++;
+}
+}
+if(c!=0)
+printf("YES");
+else
+printf("NO");
+}

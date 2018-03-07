5eb000e3ef5ed3d168169e44f0b48f0f1cf72ec5
Harigriffin <35764510+Harigriffin@users.noreply.github.com>
2018-03-07 14:14:05 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int i;
+char a[100];
+scanf("%s",&a);
+i=strlen(a);
+if(i%2==0)
+{
+a[i/2]='*';
+a[(i/2)-1]='*';
+}
+else
+{
+a[i/2]='*';
+}
+printf("%s",a);
+}
